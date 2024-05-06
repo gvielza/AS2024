5dd5e1038a9ebaa32e2ca57567a6753afa0cab0a
Gionnelly Vielza Durán <80008338+gvielza@users.noreply.github.com>
2024-05-06 23:50:32 +0000

    agregar mi repo y clase Automovil

---
+++ main.py
@@ -0,0 +1,12 @@
+class Automovil:
+  ruedas=4
+  def __init__(self,color,marca,aceleracion,velocidad):
+    self.color=color
+    self.marca=marca    
+    self.aceleracion=aceleracion
+    self.velocidad=velocidad
+au=Automovil()
+print(au.ruedas)
+
+
+  
